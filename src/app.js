5655c8db98687209d942e74008cd711d3718c5b4
Rafael Audy Glanzner <rafael.audy@gmail.com>
2019-08-15 11:29:32 +0800

    Adds props

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = props => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h3", {}, "Havanese")
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed)
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, {
+      name: "Gandalf",
+      animal: "cat",
+      breed: "Mixed"
+    }),
+    React.createElement(Pet, {
+      name: "Alfi",
+      animal: "dog",
+      breed: "Weimaraner"
+    }),
+    React.createElement(Pet, {
+      name: "Peny",
+      animal: "dog",
+      breed: "Pug"
+    })
   ]);
 };
 
