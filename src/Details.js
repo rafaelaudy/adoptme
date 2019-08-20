1fb5259456556a14ecbfe4b75bcd84505a624a90
Rafael Audy Glanzner <rafael.audy@gmail.com>
2019-08-20 18:43:52 +0100

    Add details as class component

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,31 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
 
-const Details = () => <div>Fatils</div>;
+  componentDidMount() {
+    const { id } = this.props;
+
+    pet.animal(id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        breed: animal.breeds.primary,
+        media: animal.photos,
+        loading: false
+      });
+    });
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    return loading ? <div>loading</div> : <div>Info</div>;
+  }
+}
 
 export default Details;
