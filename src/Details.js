29d3ecb7d3f4ba0b91757b9b114fa6a2c27465fb
Rafael Audy Glanzner <rafael.audy@gmail.com>
2019-08-21 13:21:53 +0100

    Show pet details

--- src/Details.js
+++ src/Details.js
@@ -16,15 +16,27 @@ class Details extends React.Component {
         location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
         breed: animal.breeds.primary,
         media: animal.photos,
+        description: animal.description,
         loading: false
       });
     });
   }
 
   render() {
-    const { loading } = this.state;
+    const { loading, name, animal, location, breed, description } = this.state;
 
-    return loading ? <div>loading</div> : <div>Info</div>;
+    return loading ? (
+      <h1>loading ...</h1>
+    ) : (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt me</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
